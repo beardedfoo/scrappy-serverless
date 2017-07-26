aabcdd95a2a08c3e80705334531da2c6bcebb7c0
Cyle Riggs <beardedfoo@gmail.com>
2017-07-26 13:40:24 +0200

    Add plan note

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from flask import Flask, Response, request
 
 app = Flask(__name__)
 
-# PLAN: Connect to docker
+# PLAN: Connect to docker & test connection (docker info)
 
 @app.route('/healthz')
 def healthz():
