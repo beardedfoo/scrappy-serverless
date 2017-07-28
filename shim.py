13c266afa8e5b8d404230076b1d58939e57133d8
Cyle Riggs <beardedfoo@gmail.com>
2017-07-28 15:40:22 +0200

    Remove shim.py, use rq as shim instead
    New plan is to use rq worker instead.

--- shim.py
+++
@@ -1,9 +0,0 @@
-# pylint: disable=invalid-name,unused-import
-
-# PLAN: Call as: curl shim.py | python -
-
-# PLAN: Get function parameters as JSON argv[1]
-
-# PLAN: Get function name as argv[2]
-
-# PLAN: Return web page result as JSON on stdout
